615165d6be159306452b5e69c0a801710be5c6e6
gaohanghang <1341947277@qq.com>
2019-01-24 20:46:39 +0800

    <添加|修改>(java): 添加FlatSwing Swing美化jar包，添加测试类

--- src/test/java/MacUiTest.java
+++ src/test/java/MacUiTest.java
@@ -1,7 +1,7 @@
 import com.gaohanghang.MacUiFrame;
 
 /**
- * @Description:
+ * @Description: 仿Mac的样式效果测试
  * @author: Gao Hang Hang
  * @date 2019/01/23 23:58
  */
---
+++ src/test/java/MyTest.java
@@ -0,0 +1,12 @@
+import com.mommoo.example.ExampleFactory;
+
+/**
+ * @Description:
+ * @author: Gao Hang Hang
+ * @date 2019/01/24 18:47
+ */
+public class MyTest {
+    public static void main(String[] args) {
+        ExampleFactory.FlatTextFieldExample.example1();
+    }
+}
